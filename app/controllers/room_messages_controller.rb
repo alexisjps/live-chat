f7b94d4f95e075d7e2b42c550e66aee4aa6f1e1a
alexis <alexis@hellomybusiness.fr>
2020-12-02 00:20:48 +0100

    not finish

--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -5,7 +5,8 @@ class RoomMessagesController < ApplicationController
       @room_message = RoomMessage.create user: current_user,
                                          room: @room,
                                          message: params.dig(:room_message, :message)
-                                         RoomChannel.broadcast_to @room, @room_message
+  
+     RoomChannel.broadcast_to @room, @room_message
     end
   
     protected
@@ -13,4 +14,4 @@ class RoomMessagesController < ApplicationController
     def load_entities
       @room = Room.find params.dig(:room_message, :room_id)
     end
-  end
+end
